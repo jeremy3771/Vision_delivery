0b3bf12f19e5faed297d1ce304585d4d0e6f2572
Kim <jeremy3771@naver.com>
2024-10-20 03:30:02 +0900

    Update run_opencm.hpp

--- delivery_motion/include/delivery_motion/run_opencm.hpp
+++ delivery_motion/include/delivery_motion/run_opencm.hpp
@@ -10,6 +10,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "std_msgs/msg/int8_multi_array.hpp"
 #include "delivery_motion/motor_driver.hpp"
 
 #define PI 3.141592653589793
@@ -19,10 +20,13 @@ public:
     DynamixelController();
 private:
     void twist_cb(const geometry_msgs::msg::Twist msg);
+    void gimbal_callback(const std_msgs::msg::Int8MultiArray::SharedPtr msg);
     void timer_cb();
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub_;
+    rclcpp::Subscription<std_msgs::msg::Int8MultiArray>::SharedPtr gimbal_sub_;
     rclcpp::TimerBase::SharedPtr timer_;
-    int64_t motPos_[4] = {3073, };
+    int64_t yawPos_[4] = {2048, };
+    int64_t gimbalPos_[4] = {2048, };
     double wheelOffset1_, wheelOffset2_, axleWidth_;
 };
 
