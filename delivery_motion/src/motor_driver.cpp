92b56a2d196d726c8a599182cec8cc846770e4a7
Kim <jeremy3771@naver.com>
2024-10-20 03:31:46 +0900

    Update motor_driver.cpp

--- delivery_motion/src/motor_driver.cpp
+++ delivery_motion/src/motor_driver.cpp
@@ -29,6 +29,7 @@ bool MotorCommand::setTorque(bool enable) {
     uint8_t dxl_error = 0;
     int dxl_comm_result = COMM_TX_FAIL;
 
+    // wheel_in Dynamixels
     dxl_comm_result = packetHandler_->write1ByteTxRx(portHandler_, DXL_F_LEFT_ID, ADDR_X_TORQUE_ENABLE, enable, &dxl_error);
     if(dxl_comm_result != COMM_SUCCESS)
         return false;
@@ -53,10 +54,35 @@ bool MotorCommand::setTorque(bool enable) {
     else if(dxl_error != 0)
         return false;
 
+    // Gimbal Dynamixels
+    dxl_comm_result = packetHandler_->write1ByteTxRx(portHandler_, DXL_GIMBAL1_ID, ADDR_X_TORQUE_ENABLE, enable, &dxl_error);
+    if(dxl_comm_result != COMM_SUCCESS)
+        return false;
+    else if(dxl_error != 0)
+        return false;
+
+    dxl_comm_result = packetHandler_->write1ByteTxRx(portHandler_, DXL_GIMBAL2_ID, ADDR_X_TORQUE_ENABLE, enable, &dxl_error);
+    if(dxl_comm_result != COMM_SUCCESS)
+        return false;
+    else if(dxl_error != 0)
+        return false;
+
+    dxl_comm_result = packetHandler_->write1ByteTxRx(portHandler_, DXL_GIMBAL3_ID, ADDR_X_TORQUE_ENABLE, enable, &dxl_error);
+    if(dxl_comm_result != COMM_SUCCESS)
+        return false;
+    else if(dxl_error != 0)
+        return false;
+
+    dxl_comm_result = packetHandler_->write1ByteTxRx(portHandler_, DXL_GIMBAL4_ID, ADDR_X_TORQUE_ENABLE, enable, &dxl_error);
+    if(dxl_comm_result != COMM_SUCCESS)
+        return false;
+    else if(dxl_error != 0)
+        return false;
+
     return true;
 }
 
-bool MotorCommand::writePosition(const int64_t value[4]) {
+bool MotorCommand::writeYawPosition(const int64_t value[4]) {
     bool dxl_addparam_result;
     int8_t dxl_comm_result;
     uint8_t data_byte[4] = {0, };
@@ -90,6 +116,51 @@ bool MotorCommand::writePosition(const int64_t value[4]) {
     data_byte[2] = DXL_LOBYTE(DXL_HIWORD(value[3]));
     data_byte[3] = DXL_HIBYTE(DXL_HIWORD(value[3]));
     dxl_addparam_result = groupSyncWritePosition_->addParam(DXL_R_RIGHT_ID, (uint8_t*)&data_byte);
+    if (dxl_addparam_result != true)
+        return false;
+
+    dxl_comm_result = groupSyncWritePosition_->txPacket();
+    if (dxl_comm_result != COMM_SUCCESS)
+        return false;
+
+    groupSyncWritePosition_->clearParam();
+    return true;
+}
+
+bool MotorCommand::writeGimbalPosition(const int64_t value[4]) {
+    bool dxl_addparam_result;
+    int8_t dxl_comm_result;
+    uint8_t data_byte[4] = {0, };
+
+    data_byte[0] = DXL_LOBYTE(DXL_LOWORD(value[0]));
+    data_byte[1] = DXL_HIBYTE(DXL_LOWORD(value[0]));
+    data_byte[2] = DXL_LOBYTE(DXL_HIWORD(value[0]));
+    data_byte[3] = DXL_HIBYTE(DXL_HIWORD(value[0]));
+    dxl_addparam_result = groupSyncWritePosition_->addParam(DXL_GIMBAL1_ID, (uint8_t*)&data_byte);
+    if (dxl_addparam_result != true)
+        return false;
+
+    data_byte[0] = DXL_LOBYTE(DXL_LOWORD(value[1]));
+    data_byte[1] = DXL_HIBYTE(DXL_LOWORD(value[1]));
+    data_byte[2] = DXL_LOBYTE(DXL_HIWORD(value[1]));
+    data_byte[3] = DXL_HIBYTE(DXL_HIWORD(value[1]));
+    dxl_addparam_result = groupSyncWritePosition_->addParam(DXL_GIMBAL2_ID, (uint8_t*)&data_byte);
+    if (dxl_addparam_result != true)
+        return false;
+
+    data_byte[0] = DXL_LOBYTE(DXL_LOWORD(value[2]));
+    data_byte[1] = DXL_HIBYTE(DXL_LOWORD(value[2]));
+    data_byte[2] = DXL_LOBYTE(DXL_HIWORD(value[2]));
+    data_byte[3] = DXL_HIBYTE(DXL_HIWORD(value[2]));
+    dxl_addparam_result = groupSyncWritePosition_->addParam(DXL_GIMBAL3_ID, (uint8_t*)&data_byte);
+    if (dxl_addparam_result != true)
+        return false;
+
+    data_byte[0] = DXL_LOBYTE(DXL_LOWORD(value[3]));
+    data_byte[1] = DXL_HIBYTE(DXL_LOWORD(value[3]));
+    data_byte[2] = DXL_LOBYTE(DXL_HIWORD(value[3]));
+    data_byte[3] = DXL_HIBYTE(DXL_HIWORD(value[3]));
+    dxl_addparam_result = groupSyncWritePosition_->addParam(DXL_GIMBAL4_ID, (uint8_t*)&data_byte);
     if (dxl_addparam_result != true)
         return false;
 
