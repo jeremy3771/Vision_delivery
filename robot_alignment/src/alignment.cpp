f9f5d5a5d831a87b18a540d98e1abff7995f8c8c
Kim <jeremy3771@naver.com>
2024-10-20 16:50:32 +0900

    Update alignment.cpp

--- robot_alignment/src/alignment.cpp
+++ robot_alignment/src/alignment.cpp
@@ -25,20 +25,29 @@ public:
 
 private:
     void timer_cb() {
-        printf("Front Dist: %f, Left: %f, Right: %f, Angle: %f\n", front_dist_, left_dist_, right_dist_, angle_);
+        printf("Front Dist: %.3f, Left: %.3f, Right: %.3f, Angle: %.3f\n", front_dist_, left_dist_, right_dist_, angle_);
     }
     void scan_cb(const sensor_msgs::msg::LaserScan::ConstSharedPtr& msg) {
-		 front_dist_ = msg->ranges[0];
-		 left_dist_ = msg->ranges[89];
-		 right_dist_ = msg->ranges[269];
+		 front_dist_ = msg->ranges[179];
+		 left_dist_ = msg->ranges[269];
+		 right_dist_ = msg->ranges[89];
+
+		 // Change right and left
+		 // float right_45deg = msg->ranges[134];
+		 // float right_45deg_x = right_45deg * std::cos(45 * (PI / 180));
+		 // float right_45deg_y = right_45deg * std::sin(45 * (PI / 180));
 		 
-		 float right_45deg = msg->ranges[314];
-		 float right_45deg_x = right_45deg * std::cos(45 * (PI / 180));
-		 float right_45deg_y = right_45deg * std::sin(45 * (PI / 180));
+		 // float dx = right_45deg_x - right_dist_;
+		 // float dy = right_45deg_y;
+
+		 float left_45deg = msg->ranges[224];
+		 float left_45deg_x = left_45deg * std::cos(45 * (PI / 180));
+		 float left_45deg_y = left_45deg * std::sin(45 * (PI / 180));
 		 
-		 float dx = right_45deg_x - right_dist_;
-		 float dy = right_45deg_y;
-		 angle_ = std::atan2(dy, dx);
+		 float dx = left_45deg_x - left_dist_;
+		 float dy = left_45deg_y;
+		 
+		 angle_ = std::atan2(dy, dx) * (180 / PI) - 90;
     }
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr sub_;
